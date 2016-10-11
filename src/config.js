f20bfbddf5cca7c7c3830c33cd01a92688e30eef
alexhucy <alexhucy@126.com>
2016-10-11 17:36:38 +0800

    submit code

--- src/config.js
+++ src/config.js
@@ -1,6 +1,11 @@
-if(process.env.NODE_ENV === 'production'){
-	module.exports = require('./config.prod')
-}
-else{
-	module.exports = require('./config.dev')
+// if(process.env.NODE_ENV === 'production'){
+// 	module.exports = require('./config.prod')
+// }
+// else{
+// 	module.exports = require('./config.dev')
+// }
+
+module.exports = {
+	appid: 'wx77025818d05fc144',
+	domain: 'http://www.xingaokaowang.cn/sign'
 }
